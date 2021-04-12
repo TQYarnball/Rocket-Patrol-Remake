02071bfa8666b3ecb1bd451373f457829de37863
Riley Dix <rdix@ucsc.edu>
2021-04-12 13:24:13 -0700

    missed a comma

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,7 +10,7 @@ class Play extends Phaser.Scene{
         this.load.image('rocket', 'assets/rocket.png');
         this.load.image('spaceship', 'assets/spaceship.png');
         //load  spritesheet
-        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0 endFrame: 9});
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
     
     create(){

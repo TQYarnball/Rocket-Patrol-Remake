d0f02511583d26ce8e23479189468610f9f1cdbe
Riley Dix <rdix@ucsc.edu>
2021-04-12 14:57:52 -0700

    Menu fixed (keys were not in caps like they needed to be)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 //const { Phaser } = require("../lib/phaser");
 //remove the above line if nothing specifically tells you to add it by the end of the tutorial.
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.Auto,
     width: 640,
     height: 480,
     scene: [Menu, Play]
 }
 
-let game = new Phaser.Game();
+let game = new Phaser.Game(config);
 
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 //set up keyboard variable
-let keyLeft, keyRight, keyF, keyR;
+let keyLEFT, keyRIGHT, keyF, keyR;

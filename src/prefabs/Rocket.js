620741fa5ebb2a9eb420881f3d3f4c579663f328
Riley Dix <rdix@ucsc.edu>
2021-04-12 15:20:21 -0700

    Fixed moving right, and that somehow made the rest of the game playable??

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -12,10 +12,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        if(this.isFiring){
+        if(!this.isFiring){
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
-            } else if (keyRight.isDown && this.x <= game.config.width - borderUISize - this.width) {
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
                 this.x += this.moveSpeed;
             }
         }

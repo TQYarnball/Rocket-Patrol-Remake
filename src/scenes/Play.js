ed35dbbec1782390e32b7c6cc03802948f0bba2b
Riley Dix <rdix@ucsc.edu>
2021-04-18 20:52:07 -0700

    ui is RED

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -56,7 +56,7 @@ class Play extends Phaser.Scene{
         borderUISize + borderPadding, 
         game.config.width, 
         borderUISize * 2, 
-        0x00FF00).setOrigin(0, 0);
+        0xFF0000).setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 
         0, 

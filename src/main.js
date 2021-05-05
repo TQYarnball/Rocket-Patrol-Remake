c5122f4cebd344fe2a82819fe62934f0b03a847d
Riley Dix <rdix@ucsc.edu>
2021-05-05 15:33:39 -0700

    words

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //Riley Dix 4/17/21 -- took about 12 hours
 //Points Breakdown: 
 //Game artwork, UI, and sound has been changed for a pirate aesthetic (60 pts)
-//Planning to add mouse control (20 pts)
+//Added mouse control (20 pts)
 //Added time when ships are hit (20pts)
 let config = {
     type: Phaser.Auto,
